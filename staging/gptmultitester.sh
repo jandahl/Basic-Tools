0cc0977cd46d3252f112713d92b24577088a3ee1
Jan Gronemann <jandahl@users.noreply.github.com>
2023-04-13 09:55:35 +0200

    Replaced faulty statements

--- staging/gptmultitester.sh
+++ staging/gptmultitester.sh
@@ -1,8 +1,20 @@
 #!/bin/bash
 
+
+### ChatGPT 3.5 prompt:
+# I would like a bash script that has a multidimensional array with PORT NUMBER and PORT USAGE, e.g. "80","http";"443","https". The script should expect a target host or IPv4 address as a command line argument and give a help screen if it does not receive one. If it receives a valid command line argument, it should then proceed to: 1) Output the target on screen and do a DNS A lookup/reverse PTR lookup and print this in a parenthesis. 2) Try to reach the given target using cURL, given a separate multidimensional array. 3) For each member of its multidimensional array it should try to reach it via netcat. The script should accept several hostnames and/or IPs. Also, I should like to be able to serve this script command line arguments that add port numbers to either cURL (e.g. "-c 123,456" or "--curl 123,456") or netcat (e.g. "-p 789,123" or "--ports 789,123"), and an option for a retry timer in seconds (e.g. "-r 10" or "--retry 10"). 
+
 # Define the arrays
-declare -A CURL_PORTS=( ["80"]="http" ["443"]="https" )
-declare -A NC_PORTS=( ["22"]="ssh" ["80"]="http" ["443"]="https" )
+CURL_PORTS=("80:http" "443:https")
+NC_PORTS=("22:ssh" "80:http" "443:https")
+
+# Define message variables
+TARGET_RESOLVING_MSG="Resolving target..."
+TARGET_RESOLVED_MSG="Target resolved: "
+CURL_CHECK_MSG="Trying cURL on "
+CHECK_SUCCESS_MSG="Success!"
+CHECK_FAILURE_MSG="Failed."
+NC_CHECK_MSG="Trying netcat on "
 
 # Define functions
 function usage {
@@ -12,19 +24,21 @@ function usage {
 
 function resolve {
   local target="$1"
+  echo -n "$TARGET_RESOLVING_MSG"
   local ip=$(dig +short "$target" | tail -1)
-  echo "$target (${ip:-unknown})"
+  echo "$TARGET_RESOLVED_MSG $target (${ip:-unknown})"
 }
 
 function curl_check {
   local target="$1"
   local ports="$2"
-  for port in $(echo "$ports" | tr ',' ' '); do
-    local proto="${CURL_PORTS[$port]}"
-    if [ -n "$proto" ]; then
+  for element in "${CURL_PORTS[@]}"; do
+    local port=${element%%:*}
+    local proto=${element#*:}
+    if [[ "$ports" == *"$port"* ]]; then
       local url="$proto://$target/"
-      echo "Trying cURL on $url..."
-      curl -I -m 5 "$url" >/dev/null 2>&1 && echo "Success!" || echo "Failed."
+      echo -n "$CURL_CHECK_MSG$port... "
+      curl -I -m 5 "$url" >/dev/null 2>&1 && echo "$CHECK_SUCCESS_MSG" || echo "$CHECK_FAILURE_MSG"
     fi
   done
 }
@@ -32,11 +46,12 @@ function curl_check {
 function nc_check {
   local target="$1"
   local ports="$2"
-  for port in $(echo "$ports" | tr ',' ' '); do
-    local service="${NC_PORTS[$port]}"
-    if [ -n "$service" ]; then
-      echo "Trying netcat on $target:$port (${service})..."
-      nc -w 1 -z "$target" "$port" >/dev/null 2>&1 && echo "Success!" || echo "Failed."
+  for element in "${NC_PORTS[@]}"; do
+    local port=${element%%:*}
+    local service=${element#*:}
+    if [[ "$ports" == *"$port"* ]]; then
+      echo -n "$NC_CHECK_MSG$target:$port (${service})... "
+      nc -w 1 -z "$target" "$port" >/dev/null 2>&1 && echo "$CHECK_SUCCESS_MSG" || echo "$CHECK_FAILURE_MSG"
     fi
   done
 }
